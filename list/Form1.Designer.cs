611e9a3a435e4d84cd9c9e201eaf30d6ada79512
ollyolko <olly.olko@gmail.com>
2016-12-09 14:47:51 +0200

    ll

---
+++ list/Form1.Designer.cs
@@ -0,0 +1,314 @@
+﻿namespace list
+{
+    partial class commander
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.ListViewGroup listViewGroup1 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup2 = new System.Windows.Forms.ListViewGroup("ListViewGroup", System.Windows.Forms.HorizontalAlignment.Left);
+            System.Windows.Forms.ListViewGroup listViewGroup3 = new System.Windows.Forms.ListViewGroup("tyjty", System.Windows.Forms.HorizontalAlignment.Left);
+            this.listViewPanel1 = new System.Windows.Forms.ListView();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.btnDel = new System.Windows.Forms.Button();
+            this.btnC = new System.Windows.Forms.Button();
+            this.btnD = new System.Windows.Forms.Button();
+            this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+            this.tsPanel2 = new System.Windows.Forms.ToolStrip();
+            this.listViewPannel2 = new System.Windows.Forms.ListView();
+            this.columnHeader2Name = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2Size = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.openToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.newToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.printToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.commandToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.networkingToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.viewToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripMenuItem2 = new System.Windows.Forms.ToolStripMenuItem();
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+            this.splitContainer1.Panel1.SuspendLayout();
+            this.splitContainer1.Panel2.SuspendLayout();
+            this.splitContainer1.SuspendLayout();
+            this.menuStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // listViewPanel1
+            // 
+            this.listViewPanel1.Location = new System.Drawing.Point(12, 94);
+            this.listViewPanel1.Name = "listViewPanel1";
+            this.listViewPanel1.Size = new System.Drawing.Size(260, 549);
+            this.listViewPanel1.TabIndex = 0;
+            this.listViewPanel1.UseCompatibleStateImageBehavior = false;
+            this.listViewPanel1.View = System.Windows.Forms.View.List;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 39);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(40, 23);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "add";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // textBox
+            // 
+            this.textBox.Location = new System.Drawing.Point(13, 68);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(189, 20);
+            this.textBox.TabIndex = 2;
+            // 
+            // btnDel
+            // 
+            this.btnDel.Location = new System.Drawing.Point(58, 39);
+            this.btnDel.Name = "btnDel";
+            this.btnDel.Size = new System.Drawing.Size(32, 23);
+            this.btnDel.TabIndex = 3;
+            this.btnDel.Text = "delete";
+            this.btnDel.UseVisualStyleBackColor = true;
+            this.btnDel.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnC
+            // 
+            this.btnC.Location = new System.Drawing.Point(96, 39);
+            this.btnC.Name = "btnC";
+            this.btnC.Size = new System.Drawing.Size(53, 23);
+            this.btnC.TabIndex = 4;
+            this.btnC.Text = "c:";
+            this.btnC.UseVisualStyleBackColor = true;
+            this.btnC.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // btnD
+            // 
+            this.btnD.Location = new System.Drawing.Point(157, 39);
+            this.btnD.Name = "btnD";
+            this.btnD.Size = new System.Drawing.Size(45, 23);
+            this.btnD.TabIndex = 5;
+            this.btnD.Text = "d:";
+            this.btnD.UseVisualStyleBackColor = true;
+            this.btnD.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // splitContainer1
+            // 
+            this.splitContainer1.Location = new System.Drawing.Point(294, 27);
+            this.splitContainer1.Name = "splitContainer1";
+            this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+            // 
+            // splitContainer1.Panel1
+            // 
+            this.splitContainer1.Panel1.Controls.Add(this.tsPanel2);
+            // 
+            // splitContainer1.Panel2
+            // 
+            this.splitContainer1.Panel2.Controls.Add(this.listViewPannel2);
+            this.splitContainer1.Size = new System.Drawing.Size(353, 616);
+            this.splitContainer1.SplitterDistance = 25;
+            this.splitContainer1.TabIndex = 6;
+            // 
+            // tsPanel2
+            // 
+            this.tsPanel2.Location = new System.Drawing.Point(0, 0);
+            this.tsPanel2.Name = "tsPanel2";
+            this.tsPanel2.Size = new System.Drawing.Size(353, 25);
+            this.tsPanel2.TabIndex = 0;
+            this.tsPanel2.Text = "toolStrip1";
+            // 
+            // listViewPannel2
+            // 
+            this.listViewPannel2.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader2Name,
+            this.columnHeader2Size});
+            this.listViewPannel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.listViewPannel2.GridLines = true;
+            listViewGroup1.Header = "ListViewGroup";
+            listViewGroup1.Name = "listViewGroup1";
+            listViewGroup2.Header = "ListViewGroup";
+            listViewGroup2.Name = "listViewGroup2";
+            listViewGroup3.Header = "tyjty";
+            listViewGroup3.Name = "listViewGroup3";
+            this.listViewPannel2.Groups.AddRange(new System.Windows.Forms.ListViewGroup[] {
+            listViewGroup1,
+            listViewGroup2,
+            listViewGroup3});
+            this.listViewPannel2.Location = new System.Drawing.Point(0, 0);
+            this.listViewPannel2.Name = "listViewPannel2";
+            this.listViewPannel2.Size = new System.Drawing.Size(353, 587);
+            this.listViewPannel2.TabIndex = 0;
+            this.listViewPannel2.UseCompatibleStateImageBehavior = false;
+            this.listViewPannel2.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader2Name
+            // 
+            this.columnHeader2Name.Tag = "rfg";
+            this.columnHeader2Name.Text = "name";
+            this.columnHeader2Name.Width = 288;
+            // 
+            // columnHeader2Size
+            // 
+            this.columnHeader2Size.Text = "size";
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.commandToolStripMenuItem,
+            this.networkingToolStripMenuItem,
+            this.viewToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(659, 24);
+            this.menuStrip1.TabIndex = 7;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openToolStripMenuItem,
+            this.newToolStripMenuItem,
+            this.printToolStripMenuItem,
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(35, 20);
+            this.fileToolStripMenuItem.Text = "file";
+            // 
+            // openToolStripMenuItem
+            // 
+            this.openToolStripMenuItem.Name = "openToolStripMenuItem";
+            this.openToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.openToolStripMenuItem.Text = "open";
+            // 
+            // newToolStripMenuItem
+            // 
+            this.newToolStripMenuItem.Name = "newToolStripMenuItem";
+            this.newToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.newToolStripMenuItem.Text = "new";
+            // 
+            // printToolStripMenuItem
+            // 
+            this.printToolStripMenuItem.Name = "printToolStripMenuItem";
+            this.printToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.printToolStripMenuItem.Text = "print";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(101, 22);
+            this.exitToolStripMenuItem.Text = "exit";
+            // 
+            // commandToolStripMenuItem
+            // 
+            this.commandToolStripMenuItem.Name = "commandToolStripMenuItem";
+            this.commandToolStripMenuItem.Size = new System.Drawing.Size(74, 20);
+            this.commandToolStripMenuItem.Text = "command";
+            // 
+            // networkingToolStripMenuItem
+            // 
+            this.networkingToolStripMenuItem.Name = "networkingToolStripMenuItem";
+            this.networkingToolStripMenuItem.Size = new System.Drawing.Size(79, 20);
+            this.networkingToolStripMenuItem.Text = "networking";
+            // 
+            // viewToolStripMenuItem
+            // 
+            this.viewToolStripMenuItem.Name = "viewToolStripMenuItem";
+            this.viewToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+            this.viewToolStripMenuItem.Text = "view";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripMenuItem2});
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(50, 20);
+            this.aboutToolStripMenuItem.Text = "about";
+            // 
+            // toolStripMenuItem2
+            // 
+            this.toolStripMenuItem2.Name = "toolStripMenuItem2";
+            this.toolStripMenuItem2.Size = new System.Drawing.Size(79, 22);
+            this.toolStripMenuItem2.Text = "?";
+            // 
+            // commander
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 650);
+            this.Controls.Add(this.splitContainer1);
+            this.Controls.Add(this.btnD);
+            this.Controls.Add(this.btnC);
+            this.Controls.Add(this.btnDel);
+            this.Controls.Add(this.textBox);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.listViewPanel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "commander";
+            this.Text = "commander";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.splitContainer1.Panel1.ResumeLayout(false);
+            this.splitContainer1.Panel1.PerformLayout();
+            this.splitContainer1.Panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+            this.splitContainer1.ResumeLayout(false);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listViewPanel1;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button btnDel;
+        private System.Windows.Forms.Button btnC;
+        private System.Windows.Forms.Button btnD;
+        private System.Windows.Forms.SplitContainer splitContainer1;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem commandToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem networkingToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem viewToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.ToolStrip tsPanel2;
+        private System.Windows.Forms.ToolStripMenuItem openToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem newToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem printToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem toolStripMenuItem2;
+        private System.Windows.Forms.ListView listViewPannel2;
+        private System.Windows.Forms.ColumnHeader columnHeader2Name;
+        private System.Windows.Forms.ColumnHeader columnHeader2Size;
+    }
+}
+
---
+++ list/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace list
+{
+    public partial class commander : Form
+    {
+        public commander()
+        {
+            InitializeComponent();
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e)
+        {
+            listViewPanel1.Items.Add(textBox.Text);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            foreach (ListViewItem currentItem in listViewPanel1.SelectedItems)
+            {
+                listViewPanel1.Items.Remove(currentItem);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            show("c:\\");
+        }
+
+        public void show(string str)
+        {
+            listViewPanel1.Clear();
+            var folder = Directory.GetDirectories(str);
+            foreach (var fol in folder)
+            {
+                listViewPanel1.Items.Add(fol);
+            }
+
+            var file = Directory.GetFiles(str);
+            foreach (var ste in file)
+            {
+                listViewPanel1.Items.Add(ste);
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            show("d:\\");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var drivers = Environment.GetLogicalDrives();
+            foreach (var item in drivers)
+            {
+                
+                ToolStripButton items = new ToolStripButton();
+                items.Text = item.ToString();
+                items.DisplayStyle=ToolStripItemDisplayStyle.ImageAndText;
+                items.Image =Image.FromFile("..\\..\\hard.ico");
+                //items.Click += toolStripBotton_CLick();
+                items.Click+=new EventHandler(toolStripBotton_CLick);
+                tsPanel2.Items.Add(items);
+            }
+        }
+
+        private void toolStripBotton_CLick(object sender, EventArgs e)
+        {
+            show("d:\\");
+        }
+    }
+}
